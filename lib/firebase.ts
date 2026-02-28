b8b733333b87cff417dcd3243e3795c1e7664aae
Sam Bradley <77120515+SammyBJames@users.noreply.github.com>
2026-02-27 23:50:14 -0700

    Add firebase db, auth, app references

---
+++ lib/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyADoaQwFl3086AVzGCEa891f3VZTAoWYn0",
+  authDomain: "hackusu-repair-manager.firebaseapp.com",
+  projectId: "hackusu-repair-manager",
+  storageBucket: "hackusu-repair-manager.firebasestorage.app",
+  messagingSenderId: "475239334487",
+  appId: "1:475239334487:web:c703d133c60a04d06a9912"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
