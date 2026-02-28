fe5664c1b463c6fa11e7a2c3753d7a063ba3f7a7
Nate Washburn <nathanprattwashburn@gmail.com>
2026-02-28 03:59:43 -0700

    fixed layout

--- app/[company]/layout.tsx
+++ app/[company]/layout.tsx
@@ -15,7 +15,7 @@ export default async function InternalLayout({ children, params }: {children: Re
             <div className="flex flex-col flex-1">
               <MainHeader company={company} />
 
-              <main className="flex-1 p-6 overflow-auto">
+              <main className="flex-1 overflow-auto">
                 {children}
               </main>
             </div>
